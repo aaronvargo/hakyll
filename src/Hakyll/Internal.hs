322c3b02f18b255830f8337e1966ed8966a413e5
Aaron Vargo <aaronvargo@users.noreply.github.com>
2016-12-14 11:58:49 -0800

    Expose some internals

---
+++ src/Hakyll/Internal.hs
@@ -0,0 +1,10 @@
+module Hakyll.Internal
+  ( parsePage
+  , Compiler(..)
+  , compilerAsk
+  , CompilerRead
+  , CompilerResult
+  ) where
+
+import Hakyll.Core.Provider.Metadata
+import Hakyll.Core.Compiler.Internal
